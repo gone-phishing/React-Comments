df5bb565324149d29251e309c7f40a3bac7746f9
Ritesh Kumar Singh <riteshoneinamillion@gmail.com>
2018-07-01 18:44:43 +0530

    Adding routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import CommentBox from './CommentBox.jsx';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<CommentBox parent_id={223} />, document.getElementById('root'));
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+ReactDOM.render(
+		<Router> 
+			<Switch> <Route basename={process.env.PUBLIC_URL} path={'/'} render={ (routerProps) => <CommentBox parent_id={223}  routerProps={routerProps} />} /> </Switch>
+        </Router>, document.getElementById('root'));
 registerServiceWorker();
